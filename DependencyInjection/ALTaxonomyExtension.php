d986eec23a95d3d3f49c9e8ad145eb1cfede5987
Bez Hermoso <bez@activelamp.com>
2014-07-01 17:35:15 -0700

    DI config.

--- DependencyInjection/ALTaxonomyExtension.php
+++ DependencyInjection/ALTaxonomyExtension.php
@@ -3,8 +3,11 @@
 namespace ActiveLAMP\Bundle\TaxonomyBundle\DependencyInjection;
 
 use ActiveLAMP\Bundle\TaxonomyBundle\DependencyInjection\Configuration;
+use Doctrine\ORM\Tools\ResolveTargetEntityListener;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -27,6 +30,18 @@ class ALTaxonomyExtension extends Extension
         $loader->load('services.yml');
         $loader->load('forms.yml');
 
+        $this->prepareTaxonomyLoader($container);
+        $this->prepareTaxonomies($config, $container);
+
+
+        //exit;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    protected function prepareTaxonomyLoader(ContainerBuilder $container)
+    {
         $bundles = $container->getParameter('kernel.bundles');
 
         $files = array();
@@ -40,6 +55,64 @@ class ALTaxonomyExtension extends Extension
 
         $definition = $container->getDefinition('al_taxonomy.taxonomy_loader');
         $definition->replaceArgument(0, $files);
+    }
 
+    public function prepareTaxonomies(array $config, ContainerBuilder $container)
+    {
+        foreach ($config['taxonomies'] as $em => $taxonomy) {
+            $this->prepareTaxonomy($taxonomy, $em, $container);
+        }
+    }
+
+    public function prepareTaxonomy(array $taxonomyConfig, $emName, ContainerBuilder $container)
+    {
+        $defaults = array(
+            'vocabulary_class' => $container->getParameter('al_taxonomy.entity.vocabulary.class'),
+            'term_class' => $container->getParameter('al_taxonomy.entity.term.class'),
+            'entity_term_class' => $container->getParameter('al_taxonomy.entity.entity_term.class'),
+            'taxonomy_service_class' => $container->getParameter('al_taxonomy.taxonomy_service.class'),
+        );
+
+        $taxonomyConfig = array_replace_recursive($defaults, array_filter($taxonomyConfig));
+
+        $service = new Definition($taxonomyConfig['taxonomy_service_class']);
+        $service->setArguments(array(
+            new Reference(sprintf('doctrine.orm.%s_entity_manager', $emName)),
+            $defaults['vocabulary_class'],
+            $defaults['term_class'],
+            $defaults['entity_term_class'],
+        ));
+
+        $this->attachResolveTargetListener($taxonomyConfig, $emName, $container);
+
+        $container->setDefinition(sprintf('al_taxonomy.taxonomy_service.%s', $emName), $service);
+
+    }
+
+    /**
+     * @param array $config
+     * @param $emName
+     * @param ContainerBuilder $container
+     */
+    protected function attachResolveTargetListener(array $config, $emName, ContainerBuilder $container)
+    {
+        $definition = new Definition('Doctrine\ORM\Tools\ResolveTargetEntityListener');
+        $definition->addMethodCall('addResolveTargetEntity', array(
+            'ActiveLAMP\\Taxonomy\\Entity\\VocabularyInterface',
+            $config['vocabulary_class'],
+            array(),
+        ));
+        $definition->addMethodCall('addResolveTargetEntity', array(
+            'ActiveLAMP\\Taxonomy\\Entity\\TermInterface',
+            $config['term_class'],
+            array(),
+        ));
+        $definition->addMethodCall('addResolveTargetEntity', array(
+            'ActiveLAMP\\Taxonomy\\Entity\\EntityTermInterface',
+            $config['entity_term_class'],
+            array(),
+        ));
+
+        //$em = $container->getDefinition(sprintf('doctrine.orm.%s_entity_manager', $emName));
     }
 }
